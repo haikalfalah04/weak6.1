cf1c8d70cbdd2feea98825cd77d38e3face45feb
Haikal Falah <haikalfalah04@gmail.com>
2025-11-06 00:31:20 +0700

    second commit

---
+++ lib/screen/splash_screen_3.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:splash_screen/screen/login_screen.dart';
+
+class MySplashScreen3 extends StatelessWidget {
+  const MySplashScreen3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+        children: [
+          SizedBox(height: 50),
+          Container(
+            width: 240,
+            height: 240,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.amber,
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/image3.jpeg"),
+                    fit: BoxFit.cover,
+                  ),
+            ),
+          ),
+          const SizedBox(height: 20),
+
+              // 🔹 Judul
+              const Text(
+                "Welcome 3",
+                style: TextStyle(
+                  fontSize: 32.0,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              const SizedBox(height: 10),
+
+              // 🔹 Subtitle 2 baris
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                child: Text(
+                  "Forgot to bring your wallet when you are shopping?\nDon’t worry, we’ve got you covered!",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black54,
+                    height: 1.5,
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 40),
+
+              // 🔹 3 Bullet Indicator
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 15,
+                    height: 15,
+                    margin: const EdgeInsets.symmetric(horizontal: 4),
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Color(0xFFedede9), // aktif
+                    ),
+                  ),
+                  Container(
+                    width: 15,
+                    height: 15,
+                    margin: const EdgeInsets.symmetric(horizontal: 4),
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Color(0xFFedede9), // non-aktif
+                    ),
+                  ),
+                  Container(
+                    width: 15,
+                    height: 15,
+                    margin: const EdgeInsets.symmetric(horizontal: 4),
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Color(0xFF84a98c), // non-aktif
+                    ),
+                  ),
+                ],
+              ),
+
+              const Spacer(),
+
+              // 🔹 Tombol Continue
+              SizedBox(height: 20),
+              Container(
+                margin: const EdgeInsets.only(left: 40, right: 40),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 55,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 11, 231, 59),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    onPressed: () {
+                    // ✅ Pindah ke halaman login dan hapus semua route sebelumnya
+                      Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const LoginScreen(),
+                        ),
+                        (Route<dynamic> route) => false,
+                      );  
+                    },
+                    child: const Text(
+                      "Continue",
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 70),
+
+        ],
+      ),
+        ),
+      ),
+    );
+  }
+}
